f28df636dd0659b336098630e1f9e7f63ee22eb5
ShirinAjou <ajoushirin@gmail.com>
2026-04-21 22:21:58 +0200

    worked on tests

--- backend/test/test_usercontroller.py
+++ backend/test/test_usercontroller.py
@@ -1,39 +1,42 @@
 import pytest
 from unittest.mock import patch, MagicMock
-
 from src.util.daos import getDao
 from src.controllers.usercontroller import UserController as usercontroller
 
 class TestEmailLogin:
-   
-   @pytest.fixture
-   def dao_find():
-        yield DAO()
+#    @pytest.fixture
+#    def dao_find():
+#         yield DAO()
 
-    @pytest.fixture
-   def mock_response():
-        yield {
-            "email":email
-        }
+#     @pytest.fixture
+#    def mock_response():
+#         yield {
+#             "email":email
+#         }
 
-    @pytest.mark.namespaces
-    def test_email(self, mocker):
-        user = {"email":email}
+#     @pytest.mark.namespaces
+#     def test_email(self, mocker):
+#         user = {"email":email}
 
-        mocked_user = mocker.patch("getDao", return_value=user)
+#         mocked_user = mocker.patch("getDao", return_value=user)
 
-        email = "test@email.com"
+#         email = "test@email.com"
         
-        result = usercontroller.get_user_by_email(email)
-        assert result is user
-        mocked_user.assert_called_once()
-
-
+#         result = usercontroller.get_user_by_email(email)
+#         assert result is user
+#         mocked_user.assert_called_once()
 
     @pytest.mark.namespaces
     def test_multiple_users(self):
-        thisdict = [
-            {"user_one": "test@email.com"},
-            {"user_two": "test@email.com"},
-            {"user_three": "test@email.com"}
+        dao_mock = MagicMock()
+        user_c = usercontroller(dao_mock)
+
+        test_users = [
+            {"name": "user_one", "email": "test@email.com"},
+            {"name": "user_two", "email": "test@email.com"},
+            {"name": "user_three", "email": "test@email.com"},
         ]
+
+        user_c.dao.find.return_value = test_users
+        result = user_c.get_user_by_email("test@email.com")
+        assert result == test_users[0]
