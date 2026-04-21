034b750728d87bf3540f02ed8598719ef23beee1
ShirinAjou <ajoushirin@gmail.com>
2026-04-22 00:18:23 +0200

    worked on tests

--- backend/test/test_usercontroller.py
+++ backend/test/test_usercontroller.py
@@ -27,16 +27,18 @@ class TestEmailLogin:
 #         mocked_user.assert_called_once()
 
     @pytest.mark.namespaces
-    def test_multiple_users(self):
+    def test_multiple_users(self, capsys):
         dao_mock = MagicMock()
         user_c = usercontroller(dao_mock)
 
         test_users = [
             {"name": "user_one", "email": "test@email.com"},
             {"name": "user_two", "email": "test@email.com"},
-            {"name": "user_three", "email": "test@email.com"},
         ]
 
         user_c.dao.find.return_value = test_users
         result = user_c.get_user_by_email("test@email.com")
+        captured = capsys.readouterr()
+
         assert result == test_users[0]
+        assert "more than one user found" in captured.out.lower()
